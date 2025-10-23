16609cd7579b75c122b07bacf45c5e51fd3d030b
davidcarton <david.carton@gmail.com>
2025-10-23 14:40:49 +0200

    revision 14:40 23/10/25

--- ejercicios_entregar/ejercicio_3/tareas.js
+++ ejercicios_entregar/ejercicio_3/tareas.js
@@ -4,14 +4,18 @@ const taskList = document.getElementById("taskList");
 
 // Solicitar permiso para notificaciones
 if (Notification.permission !== "granted") {
+  // API del navegador(Web Notification API)
+  // Ventana tipo alert para conceder permisos
   Notification.requestPermission();
 }
 
-// Cargar tareas guardadas
+// Cargar tareas guardadas en localStorage
+//vendria bien un boton para lipiar las tareas terminadas
 const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-savedTasks.forEach((task) => addTask(task.text, task.completed));
+savedTasks.forEach((task) => addTask(task.text, task.completed)); // Guardamos la tarea y su estado (true/false)
 
-// Añadir tarea
+// Añadir tarea, comprobamos que no este vacio el taskInput
+// .trim para quitar los espacios vacios
 addBtn.addEventListener("click", () => {
   const text = taskInput.value.trim();
   if (text !== "") {
@@ -20,10 +24,10 @@ addBtn.addEventListener("click", () => {
   }
 });
 
-// Crear tarea y añadirla a la lista
+// Crear tarea y añadirla a la lista, ademas de añadirle la class de css
 function addTask(text, completed = false) {
-  const li = document.createElement("li");
-  li.textContent = text;
+  const li = document.createElement("li"); // creamos elemento <li></li>
+  li.textContent = text; // Añadimos el texto del taskInput
   if (completed) li.classList.add("completed");
 
   li.addEventListener("click", () => toggleTask(li));
@@ -31,21 +35,23 @@ function addTask(text, completed = false) {
   saveTasks();
 }
 
-// Marcar / desmarcar tarea
+// interactuamos con completed true/false para tachada o no
 function toggleTask(li) {
   li.classList.toggle("completed");
   saveTasks();
 
   if (li.classList.contains("completed")) {
-    sendNotification(`Tarea completada: "${li.textContent}"`);
+    sendNotification(`Tarea completada: "${li.textContent}"`); // Notificacion de tarea completada
   }
 }
 
-// Guardar en localStorage
+// La alamcenamos en localStorage para que cuando recarguemos la pagina sigan apareciendo las tareas
+// mantener estados de completed
 function saveTasks() {
   const tasks = [];
   document.querySelectorAll("#taskList li").forEach((li) => {
     tasks.push({
+      // añadimos al final
       text: li.textContent,
       completed: li.classList.contains("completed"),
     });
@@ -53,7 +59,8 @@ function saveTasks() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-// Enviar notificación
+// Enviar notificación, revisa si han sido validados los permisos de notificacion
+// notificaciones al completar tarea
 function sendNotification(message) {
   if (Notification.permission === "granted") {
     new Notification("Lista de tareas", {
